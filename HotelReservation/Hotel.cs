a692e18d8aab5e27e985070143be21f066ac8726
Administrator <siddhiseth3009@gmail.com>
2020-11-02 22:14:47 +0530

    [Siddhi]Add. FindHotel method added

--- HotelReservation/Hotel.cs
+++ HotelReservation/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace HotelReservation
@@ -38,8 +39,29 @@ namespace HotelReservation
             }
         }
         /// <summary>
-        /// Default constructor
+        /// Finds rate of stay from startdate to end date at hotel
         /// </summary>
-        public Hotel() { }
+        /// <param name="startDateString"></param>
+        /// <param name="endDateString"></param>
+        /// <returns>rate of stay</returns>
+        public double FindRate(string startDateString, string endDateString)
+        {
+            double rate = 0;
+            try
+            {
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                DateTime startDate = Convert.ToDateTime(startDateString);
+                DateTime endDate = Convert.ToDateTime(endDateString);
+                for (; startDate <= endDate; startDate = startDate.AddDays(1))
+                {
+                    rate = rate + RATE;
+                }
+            }
+            catch (Exception)
+            {
+                throw new HotelReservationException(HotelReservationException.ExceptionType.INVALID_DATE, "Invalid date entered");
+            }
+            return rate;
+        }
     }
 }
--- HotelReservation/Program.cs
+++ HotelReservation/Program.cs
@@ -7,6 +7,12 @@ namespace HotelReservation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation System");
+            Console.WriteLine("Enter start date of your stay in dd/mm/yyyy format");
+            string startDate = Console.ReadLine();
+            Console.WriteLine("Enter end date of your stay in dd/mm/yyyy format");
+            string endDate = Console.ReadLine();
+            CustomerServices customerService = new CustomerServices();
+            customerService.FindCheapHotel(startDate, endDate);
         }
     }
 }
