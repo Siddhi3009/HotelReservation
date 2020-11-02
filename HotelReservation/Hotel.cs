63bc1b70e5bfb237cb531fdf1025d866597b2efc
Administrator <siddhiseth3009@gmail.com>
2020-11-02 21:56:52 +0530

    [Siddhi]Add. Hotels added

---
+++ HotelReservation/Hotel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation
+{
+    class Hotel
+    {
+        //Hotel name
+        HotelType type;
+        //Weekday rate of hotel
+        private readonly double RATE;
+        /// <summary>
+        /// Parameterized constructor of Hotel
+        /// </summary>
+        /// <param name="hotelType"></param>
+        public Hotel(HotelType hotelType)
+        {
+            this.type = hotelType;
+            try
+            {
+                if (hotelType.Equals(HotelType.LAKEWOOD))
+                {
+                    this.RATE = 110;
+                }
+                if (hotelType.Equals(HotelType.BRIDGEWOOD))
+                {
+                    this.RATE = 150;
+                }
+                if (hotelType.Equals(HotelType.RIDGEWOOD))
+                {
+                    this.RATE = 220;
+                }
+            }
+            catch (HotelReservationException)
+            {
+                throw new HotelReservationException(HotelReservationException.ExceptionType.INVALID_HOTEL_TYPE, "Invalid Hotel Type");
+            }
+        }
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Hotel() { }
+    }
+}
---
+++ HotelReservation/HotelType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation
+{
+    /// <summary>
+    /// Enumerator of hotel names
+    /// </summary>
+    public enum HotelType { LAKEWOOD, BRIDGEWOOD, RIDGEWOOD }
+}
